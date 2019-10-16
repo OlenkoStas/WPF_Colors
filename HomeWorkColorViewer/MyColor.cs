fc19410243c2dbf17df3fe5d3d32a6861e875865
OlenkoStas <stas310785@gmail.com>
2019-10-16 20:25:31 +0300

    Init

---
+++ HomeWorkColorViewer/MyColor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace HomeWorkColorViewer
+{
+    internal sealed class MyColor: INotifyPropertyChanged
+    {
+        private byte _alpha;
+        private byte _red;
+        private byte _green;
+        private byte _blue;
+        private string _colorHex;
+        //Конструктор по умолчанию
+        public MyColor(byte alpha = 100, byte red = 0, byte green = 0, byte blue = 0)
+        {
+            Alpha = alpha;
+            Red = red;
+            Green = green;
+            Blue = blue;
+            GetColorHex();
+        }
+
+        public byte Alpha
+        {
+            get => _alpha;
+            set
+            {
+                if (value !=_alpha )
+                {
+                    _alpha = value;
+                    GetColorHex();
+                    OnPropertyChanged(nameof(Alpha));
+                }
+            }
+        }
+        public byte Red
+        {
+            get => _red;
+            set
+            {
+                if (value != _red)
+                {
+                    _red = value;
+                    GetColorHex();
+                    OnPropertyChanged(nameof(Red));
+                }
+            }
+        }
+        public byte Green
+        {
+            get => _green;
+            set
+            {
+                if (value != _green)
+                {
+                    _green = value;
+                    GetColorHex();
+                    OnPropertyChanged(nameof(Green));
+                }
+            }
+        }
+        public byte Blue
+        {
+            get => _blue;
+            set
+            {
+                if (value != _blue)
+                {
+                    _blue = value;
+                    GetColorHex();
+                    OnPropertyChanged(nameof(Blue));
+                }
+            }
+        }
+        public string MyColorHex
+        {
+            get => _colorHex;
+            set
+            {
+                if (!value.Equals(_colorHex))
+                {
+                    _colorHex = value;
+                    OnPropertyChanged(nameof(MyColorHex));
+                }
+            }
+        }
+        /// <summary>
+        /// Создает Hex фрмат цвета
+        /// </summary>
+        private void GetColorHex()
+        {
+            MyColorHex = Color.FromArgb(((byte)Math.Round(Alpha * 2.55)), Red, Green, Blue).ToString();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ HomeWorkColorViewer/ViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace HomeWorkColorViewer
+{
+    internal sealed class ViewModel:INotifyPropertyChanged
+    {
+        private MyColor _myColor = new MyColor();
+        public MyColor MyColor
+        {
+            get => _myColor;
+            set
+            {
+                if (!value.Equals(_myColor))
+                {
+                    _myColor = value;
+                    OnPropertyChanged(nameof(MyColor));
+                }
+            }
+        }
+        
+        public ObservableCollection<MyColor> Colors { get; set; }
+
+        public ViewModel()
+        {
+            Colors = new ObservableCollection<MyColor>
+            {
+                new MyColor(100,197,0,226),
+                new MyColor(100,255,0,0),
+                new MyColor(100,255,255,0),
+                new MyColor(100,0,0,255),
+                new MyColor(100,76,218,101)
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        // команда добавления нового объекта
+        private MyCommand addCommand;
+        public MyCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new MyCommand(obj =>
+                  {
+                      Colors.Insert(0,new MyColor(_myColor.Alpha, _myColor.Red, _myColor.Green, _myColor.Blue));
+                  },
+                  (obj) =>
+                  {
+                      return Colors.FirstOrDefault(c => c.MyColorHex == _myColor.MyColorHex) == null ? true : false;
+                  }
+                  ));
+            }
+        }
+        // команда удаления объекта из списка
+        private MyCommand delCommand;
+        public MyCommand DelCommand
+        {
+            get
+            {
+                return delCommand ??
+                  (delCommand = new MyCommand(obj =>
+                  {
+                      var hex = obj as string;
+                      Colors.Remove(Colors.First(i => i.MyColorHex == hex));
+                  }
+                  ));
+            }
+        }
+    }
+}
